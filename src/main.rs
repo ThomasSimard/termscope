39972c49aca7186cccf5c38682b62b1eb9173694
ThomasSimard <thomas_simard@outlook.com>
2026-03-12 00:14:48 -0400

    graph

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use ratatui::{DefaultTerminal, Frame};
 use std::io::{self, BufRead};
 
-mod data;
-use data::parse_line;
+use ratatui::{DefaultTerminal, Frame};
+use ratatui::style::{Color, Stylize};
+use ratatui::symbols::Marker;
+use ratatui::widgets::{Axis, Chart, Dataset, GraphType};
+
+mod parser;
+use parser::parse_line;
 
 fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
@@ -25,7 +29,9 @@ fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
             }
         }
 
-        terminal.draw(render)?;
+        terminal.draw(|frame| {
+            render(frame, &dataset)
+        })?;
         
         if crossterm::event::read()?.is_key_press() {
             break Ok(());
@@ -33,6 +39,24 @@ fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
     }
 }
 
-fn render(frame: &mut Frame, dataset: Vec<(f64, f64)>) {
-    frame.render_widget("hello world", frame.area());
+fn render(frame: &mut Frame, dataset: &Vec<(f64, f64)>) {
+    let chart_dataset = Dataset::default()
+        .marker(Marker::Braille)
+        .graph_type(GraphType::Line)
+        .style(Color::Blue)
+        .data(dataset);
+
+    let x_axis = Axis::default()
+        .title("x axis".blue())
+        .bounds([0.0, 10.0])
+        .labels(["0", "5", "10"]);
+
+    let y_axis = Axis::default()
+        .title("y axis".blue())
+        .bounds([0.0, 10.0])
+        .labels(["0", "5", "10"]);
+
+    let chart = Chart::new(vec![chart_dataset]).x_axis(x_axis).y_axis(y_axis);
+
+    frame.render_widget(chart, frame.area());
 }
