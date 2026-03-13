e9146d2e8f3df10d00ec99245bb2fd86bd3f4561
ThomasSimard <thomas_simard@outlook.com>
2026-03-13 13:00:58 -0400

    auto chart style

--- src/ui.rs
+++ src/ui.rs
@@ -19,12 +19,21 @@ fn generate_axis<'a>(min_max: &MinMax) -> Axis<'a> {
         ])
 }
 
-fn generate_chart_dataset<'a>(data: &'a [(f64, f64)]) -> Dataset<'a> {
+const COLORS: [Color; 6] = [
+    Color::Blue,
+    Color::Red,
+    Color::Green,
+    Color::Yellow,
+    Color::Magenta,
+    Color::Cyan,
+];
+
+fn generate_chart_dataset<'a>(data: &'a [(f64, f64)], index: usize) -> Dataset<'a> {
     Dataset::default()
-        .name(format!("Dataset ({})", data.len()))
+        .name(format!("Dataset #{} ({})", index, data.len()))
         .marker(Marker::Braille)
         .graph_type(GraphType::Line)
-        .style(Color::Blue)
+        .style(COLORS[index])
         .data(data)
 }
 
@@ -32,11 +41,12 @@ pub fn render(frame: &mut Frame, processing: &Processing) {
     let x_axis = generate_axis(&processing.domain);
     let y_axis = generate_axis(&processing.range); 
 
-    let mut datasets = Vec::default();
-
-    for data in processing.get_data() {
-        datasets.push(generate_chart_dataset(data));
-    }
+    let datasets: Vec<_> = processing
+        .get_data()
+        .iter()
+        .enumerate()
+        .map(|(i, data)| generate_chart_dataset(data, i))
+        .collect();
 
     let chart = Chart::new(datasets).x_axis(x_axis).y_axis(y_axis);
 
